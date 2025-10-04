1d2a21e2769749e626bcc97a2a9fae935beb0302
Sandeep Miriyala <3719057+sandeepmiriyala03@users.noreply.github.com>
2025-10-04 18:55:35 +0530

    pwa

--- pwa.js
+++ pwa.js
@@ -43,17 +43,4 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
 
-    function openTab(evt, tabName) {
-      var i, tabcontent, tabbuttons;
-      tabcontent = document.getElementsByClassName("tab-content");
-      for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].classList.remove("active");
-      }
-      tabbuttons = document.getElementsByClassName("tab-button");
-      for (i = 0; i < tabbuttons.length; i++) {
-        tabbuttons[i].classList.remove("active");
-      }
-      document.getElementById(tabName).classList.add("active");
-      evt.currentTarget.classList.add("active");
-      document.getElementById("header").style.display = "flex";
-    }
+   
